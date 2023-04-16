a946bf025576ee1891861e23328bc99b36edb57e
shumkovandrey <130909961+shumkovandrey@users.noreply.github.com>
2023-04-16 18:28:13 +0500

    Add files via upload
    Version 1.0

---
+++ test.py
@@ -0,0 +1,26 @@
+from pygame import *
+from PyQt6.QtWidgets import QFileDialog, QWidget, QApplication
+
+
+file_app = QApplication([])
+file_win = QWidget()
+
+W, H = 600, 400
+sc = display.set_mode((W, H))
+display.set_caption("Моя первая программа на PyGame")
+display.set_icon(image.load("/Users/andreyshumkov/Desktop/Новая папка/1446659200_1.jpeg"))
+
+clock = time.Clock()
+FPS = 60
+
+draw.rect(sc, (255, 255, 255), (10, 10, 50, 100))
+display.flip()
+
+isRunning = True
+while isRunning:
+    for e in event.get():
+        if e.type == QUIT:
+            exit()
+
+    clock.tick(FPS)
+
